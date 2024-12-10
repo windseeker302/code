0192e6d80923127b96dbdf38c809741949c34753
windseeker302 <windseeker302@outlook.com>
2024-12-10 16:44:02 +0800

    Delete 数据解析 directory

--- 数据解析/爬取图片.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = 'https://www.henetc.edu.cn/__local/0/2D/28/A5B1CBC7B5F9265BCD2A00784F1_3CD94549_1C769.jpg?e=.jpg'
-headers = {
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
-}
-
-img_data = requests.get(url,headers=headers).content
-with open('./img.jpg','wb') as fp:
-    fp.write(img_data)
